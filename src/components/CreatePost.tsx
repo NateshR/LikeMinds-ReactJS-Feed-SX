2bbc66d27826a123b41bb9ee32bf5af0e3eb7c24
Omen Uchiha <omen@LikeMindss-MacBook-Pro-2.local>
2023-07-17 13:15:17 +0530

    added support for text posts

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,21 +1,43 @@
 import userImg from "../assets/images/user.png";
 
-import React from "react";
+import React, { useState } from "react";
 import PostOptions from "./PostOptions";
+import DialogBox from "./dialog/DialogBox";
+import CreatePostDialog from "./dialog/createPost/CreatePostDialog";
 
 const CreatePost: React.FC = () => {
+  const [openCreatePostDialog, setOpenCreatePostDialog] = useState(false);
+  function closeCreatePostDialog() {
+    setOpenCreatePostDialog(false);
+  }
+  function openCreatePostDialogBox() {
+    setOpenCreatePostDialog(true);
+  }
   return (
-    <div className="lmWrapper__feed__creatPost">
-      <div className="lmWrapper__feed__creatPost__write">
-        <div className="lmWrapper__feed__creatPost__write--userImg">
-          <img src={userImg} alt="user image" />
+    <>
+      <DialogBox
+        openCreatePostDialog={openCreatePostDialog}
+        closeCreatePostDialog={closeCreatePostDialog}
+        // children={<CreatePostDialog/>}
+      >
+        <CreatePostDialog />
+      </DialogBox>
+      <div className="lmWrapper__feed__creatPost">
+        <div className="lmWrapper__feed__creatPost__write">
+          <div className="lmWrapper__feed__creatPost__write--userImg">
+            <img src={userImg} alt="user_image" />
+          </div>
+          <div className="lmWrapper__feed__creatPost__write--inputBox">
+            <input
+              type="text"
+              placeholder="Wtire something here..."
+              onSelect={openCreatePostDialogBox}
+            />
+          </div>
         </div>
-        <div className="lmWrapper__feed__creatPost__write--inputBox">
-          <input type="text" placeholder="Wtire something here..." />
-        </div>
+        <PostOptions />
       </div>
-      <PostOptions />
-    </div>
+    </>
   );
 };
 
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,15 +1,23 @@
 import LMFeedClient from "@likeminds.community/feed-js-beta";
+import { HelperFunctionsClass } from "./helper";
 interface LMFeedClientInterface {
   initiateUser(
     userUniqueId: string,
     isGuestMember: boolean,
     username?: string
   ): Promise<any>;
+  logout(refreshToken: string): Promise<any>;
+  addPost(text: string, attachments: []): Promise<any>;
+  addPost(text: string): Promise<any>;
 }
 
-export class LMClient implements LMFeedClientInterface {
+export class LMClient
+  extends HelperFunctionsClass
+  implements LMFeedClientInterface
+{
   client: LMFeedClient;
   public constructor() {
+    super();
     this.client = LMFeedClient.setApiKey(process.env.REACT_APP_API_KEY!)
       .setPlatformCode(process.env.REACT_APP_PLATFORM_CODE!)
       .setVersionCode(parseInt(process.env.REACT_APP_VERSION_CODE!))
@@ -26,6 +34,7 @@ export class LMClient implements LMFeedClientInterface {
         userUniqueId,
         isGuest: isGuestMember,
       });
+      return this.parseDataLayerResponse(apiCallResponse);
     } catch (error) {}
   }
 
@@ -34,6 +43,21 @@ export class LMClient implements LMFeedClientInterface {
       const apiCallResponse = await this.client.logout({
         refreshToken: refreshToken,
       });
+      return this.parseDataLayerResponse(apiCallResponse);
+    } catch (error) {}
+  }
+
+  async addPost(text: string) {
+    try {
+      const validLinksArray: any[] = this.detectLinks(text);
+      if (validLinksArray?.length) {
+      } else {
+        const apiCallResponse = await this.client.addPost({
+          text: text,
+          attachments: validLinksArray,
+        });
+        return this.parseDataLayerResponse(apiCallResponse);
+      }
     } catch (error) {}
   }
 }
